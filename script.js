72c8486370aa5481239ca51fcd7c3151632322d5
p-fan <p-fantin@hotmail.com>
2025-04-27 00:23:57 -0300

    added js and some smaller adjustements

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ const btnMenu = document.querySelector('.icon-menu');
 
 const overlay = document.querySelector('.overlay');
 const nav = document.querySelector('.nav-mobile');
+const body = document.querySelector('body');
 
 btnClose.addEventListener('click', function(){
     overlay.classList.add('hidden');
     nav.classList.add('hidden');
     btnClose.classList.add('hidden');
+    body.classList.remove('overflow-hidden');
 })
 
 btnMenu.addEventListener('click', function(){
     overlay.classList.remove('hidden');
     nav.classList.remove('hidden');
     btnClose.classList.remove('hidden');
+    body.classList.add('overflow-hidden');
 })
 
